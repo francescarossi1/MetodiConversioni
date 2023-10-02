f5c88eac023c08214b9a8d3789922186839636ae
francescarossi1 <francesca.rossi@iisviolamarchesini.edu.it>
2023-10-02 18:42:10 +0200

    Aggiungere i file di progetto.

---
+++ MetodiConversioni/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodiConversioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool[] b = { true, false, true, false }; 
+            Console.WriteLine("Conversione da binario a intero:");
+            Console.WriteLine(Convert_Binario_To_Intero(b));
+            Console.WriteLine("===============");
+            int[] dp = { 10, 100, 2, 4 }; 
+            Console.WriteLine("Conversione da decimale puntato a intero:");
+            Console.WriteLine(Convert_Decimale_Puntato_To_Decimale(dp));
+            Console.ReadLine();
+        }
+        static int Convert_Binario_To_Intero(bool[] b)
+        {
+            int nIntero = 0;
+            int esponente = 0;
+            for (int i = b.Length - 1; i >= 0; i--) 
+            {
+                if (b[i])
+                {
+                    nIntero = nIntero + (int)Math.Pow(2, esponente);
+                }
+                esponente++;
+            }
+            return nIntero;
+        }
+        static int Convert_Decimale_Puntato_To_Decimale(int[] dp)
+        {
+            int nIntero = 0;
+            int esponente = 0;
+            for (int i = dp.Length - 1; i >= 0; i--)
+            {
+                nIntero = nIntero + dp[i] * (int)Math.Pow(256, esponente);
+                esponente++;
+            }
+            return nIntero;
+        }
+    }
+}
